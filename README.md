950a565049e034a3fec73946f2b92354adf4496d
Linda de Haan <linda.dehaan@hotmail.com>
2018-11-13 16:05:36 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,36 @@
+# D3
+
+## Assignment
+Make a data visualization with interaction.
+
+# Process
+### Week 1
+#### Day 1 | November 12, 2018
+
+We had an introduction of the new course: Frontend Data. We have to make a visualization of data from the OBA API and make it interactive.
+After the introduction there was a guest speaker (Emma). She talked about datavisualizations and to show her work.
+
+I searched for different data than the previous assignment to visualize.
+I looked at examples of charts with interaction that are made with D3.
+
+#### Day 2 | November 13, 2018
+
+* Bedenken wat ik wil gaan visualiseren;
+* wat voor chart ga ik gebruiken;
+* Stand-up georganiseerd door May;
+* Data in een json bestand zetten;
+* Schetsen maken van ideeën.
+
+I thought of what data I want to visualize.
+
+I want to visualize books with different languages, different genres, year of publication, publishers and the place where the books are published.
+
+I'm thinking about using a bar chart or a sunburst, but I am not sure yet.
+
+We had a stand-up organised by May. Everyone told what they think they want to visualize and what kind of chart they want to use. The other ones gave some tips or shared their thoughts about it.
+
+I put my data in a `json` file.
+
+I made sketches of the ideas I've got to visualize my data and how to make it interactive.
+
+# Sketches
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-require("dotenv").config()
+require('dotenv').config()
+//  const api = require('./oba-api.js')
+// const chalk = require('chalk')
+// const express = require('express')
+// const app = express()
+// const port = 4000
+//  const obaApi = new api({
+//   url: 'https://zoeken.oba.nl/api/v1/',
+//   key: process.env.PUBLIC
+// })
+//  // Search for method, params and than optional where you wanna find something
+// // returns first 20 items
+// // obaApi.get(endpoint, params, filterKey)
+// // possible endpoints: search (needs 'q' parameter) | details (needs a 'frabl' parameter) | availability (needs a 'frabl' parameter) | holdings/root | index/x (where x = facet type (like 'book' ))
+// // possible parameters: q, librarian, refine, sort etc. check oba api documentation for all
+// // possible filterKey: any higher order key in response object, like title returns only title objects instead of full data object
+// obaApi.get('search', {
+//   q: 'language:ger',
+//   librarian: false,
+//   refine: true,
+//   facet: ['type(book)', 'pubYear(2010)', 'genre(avonturenroman)']
+// }).then(response => {
+//    // response ends up here
+//   console.log(response)
+//    // Make server with the response on the port
+//   app.get('/', (req, res) => res.json(response))
+//   app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
+//
+// })
+
+
 
 //Packages I need
 const chalk = require("chalk")
 const express = require("express")
+const fs = require("fs")
 const app = express()
-const port = 3000
+const port = 4000
 // file with all the functions to filter the data in the api.
 const getData = require("./helpers/getData.js")
 const obaWrapper = require("node-oba-api-wrapper")
@@ -21,20 +52,28 @@ const obaApi = new obaWrapper({
 // possible filterKey: any higher order key in response object, like title returns only title objects instead of full data object
 
 // Credits: Wouter Lem
-const search = async (q, facet, page, count) => {
+const search = async (q, facet) => {
   return await obaApi.get("search", {
     q,
     librarian: true,
     refine: true,
     facet,
-    page,
     count: 10,
     // Credits: Maikel van Veen & Chelsea Doeleman
     filter: (result) => {
 			const publicationYear = getData.getPublicationYearFromResult(result)
 			const currentYear = new Date().getFullYear()
+			const language = getData.getLanguageFromResult(result)
 
-			return publicationYear >= currentYear - 8
+			return publicationYear >= currentYear -3
+			&& getData.getGenreFromResult(result)
+			&& (language === "dut"
+			|| language === "eng"
+			|| language === "ger"
+			|| language === "fre"
+			|| language === "spa"
+			)
+			//>= currentYear - 8
 		}
   })
 }
@@ -44,26 +83,46 @@ const search = async (q, facet, page, count) => {
   try {
     // Credits: Jessie Mason -> She showed me how I could filter on more than 1 facet.
     //q, facet and page
-    const disneyResults = await search ("disney", ["type(movie)"], 1)
-    const dreamworksResults = await search ("dreamworks", ["type(movie)"], 1)
-    const pixarResults = await search ("pixar", ["type(movie)"], 1)
-    if (disneyResults, dreamworksResults, pixarResults) {
-      const transformedDisneyResults = getData.getTransformedResultFromResults(disneyResults)
-      const transformedDreamworksResults = getData.getTransformedResultFromResults(dreamworksResults)
-      const transformedPixarResults = getData.getTransformedResultFromResults(pixarResults)
+		const detective = await search ("genre:detective", ["type(book)"])
+		const humor = await search ("genre:humor", ["type(book)"])
+    const sciencefiction = await search ("genre:science-fiction", ["type(book)"])
+    const stripverhaal = await search ("genre:stripverhaal", ["type(book)"])
+    const avonturenroman = await search ("genre:avonturenroman", ["type(book)"])
+		const romantischverhaal = await search ("genre:romantisch-verhaal", ["type(book)"])
 
-      console.log(transformedDisneyResults)
-      console.log(transformedDreamworksResults)
-      console.log(transformedPixarResults)
+		const results =  [...detective, ...humor, ...sciencefiction, ...stripverhaal, ...avonturenroman, ...romantischverhaal]
 
-      const dataWrapper = {
-        "results": transformedDisneyResults,
-        "results": transformedDreamworksResults,
-        "results": transformedPixarResults
-      }
+    if (results) {
 
-      app.get("/", (req, res) => res.json(dataWrapper))
+			const transformedResults = getData.getTransformedResultFromResults(results)
+      // const transformedDetectiveResults = getData.getTransformedResultFromResults(detective)
+      // const transformedHumorResults = getData.getTransformedResultFromResults(humor)
+      // const transformedScienceFictionResults = getData.getTransformedResultFromResults(sciencefiction)
+			// const transformedStripResults = getData.getTransformedResultFromResults(stripverhaal)
+      // const transformedAvontuurResults = getData.getTransformedResultFromResults(avonturenroman)
+      // const transformedRomantischResults = getData.getTransformedResultFromResults(romantischverhaal)
+
+      // console.log(transformedDetectiveResults)
+      // console.log(transformedHumorResults)
+      // console.log(transformedScienceFictionResults)
+			// console.log(transformedStripResults)
+			// console.log(transformedAvontuurResults)
+			// console.log(transformedRomantischResults)
+
+			console.log(transformedResults)
+
+      // const dataWrapper = {
+      //   "results": transformedDetectiveResults,
+      //   "results": transformedHumorResults,
+      //   "results": transformedScienceFictionResults,
+			// 	"results": transformedStripResults,
+			// 	"results": transformedAvontuurResults,
+			// 	"results": transformedRomantischResults
+      // }
+
+      app.get("/", (req, res) => res.json(transformedResults))
       app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
+			fs.writeFile('data.json', JSON.stringify(transformedResults), err => err && console.error(err))
     }
   } catch (error) {
     throw new Error(error)
