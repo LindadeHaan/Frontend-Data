da88ef10421fea6f26f02355ef3e870079347f54
Linda de Haan <linda.dehaan@hotmail.com>
2018-11-22 23:13:23 +0100

    added comments

--- groupedBarChart.js
+++ groupedBarChart.js
@@ -1,238 +1,375 @@
-// function process(d) {
-//     const processed = {
-//         language: d.language,
-//         year: d.publicationYear,
-//         genre: d.genre
-//     }
-// 	return processed
-// }
-//
-// d3.json("data.json").then(data => {
-//
-//   const dataMap = data.map(process)
-//   console.log('DataMap:', dataMap)
-//   // console.log("data", data)
-//   const languageCount = d3.nest()
-//     // .key(function(d) { return d.publicationYear})
-//     .key(function(d) { return d.genre })
-//     .key(function(d) { return d.language })
-//     .rollup(function(v) { return Number(v.length) })
-//     .entries(data)
-//     // .filter(genre => genre.key !== "Stripverhaal")
-//
-//     const objWithPercentage = languageCount.map(obj => {
-//       let total = 0
-//       obj.values.map(v => total += v.value)
-//
-//       return {
-//           ...obj,
-//           values: obj.values.map(ov => ({
-//             ...ov,
-//             percent: ov.value / total * 100
-//           })),
-//           total,
-//       }
-//     })
-//     console.log("languageCount: ", objWithPercentage)
-    // console.log(JSON.stringify(objWithPercentage));
-
 // Source: https://bl.ocks.org/syncopika/f1c9036b0deb058454f825238a95b6be
 
-d3.json("data1.json").then((languageCount) => {
+// I got a lot of help with the code and explaination of the code from:
+// Jessie, Chelsea, Maikel and Wouter
 
-var dutData = [];
-var engData = [];
-var gerData = [];
-var freData = [];
-var spaData = [];
+// Data I need to show my data in the chart
+// I tried to put it in a json file, but then the change function did not work
+// I tried to fix this myself
+// But I could not make it work and it was the night before the deadline
+const languageCount = [{
+    "genre": "Detective",
+    "language": "dut",
+    "percentage": 57.9
+  },
+  {
+    "genre": "Detective",
+    "language": "eng",
+    "percentage": 35.8
+  },
+  {
+    "genre": "Detective",
+    "language": "fre",
+    "percentage": 1.6
+  },
+  {
+    "genre": "Detective",
+    "language": "ger",
+    "percentage": 3.2
+  },
+  {
+    "genre": "Detective",
+    "language": "spa",
+    "percentage": 1.6
+  },
+  {
+    "genre": "Humor",
+    "language": "dut",
+    "percentage": 50.4
+  },
+  {
+    "genre": "Humor",
+    "language": "eng",
+    "percentage": 39.9
+  },
+  {
+    "genre": "Humor",
+    "language": "fre",
+    "percentage": 3.5
+  },
+  {
+    "genre": "Humor",
+    "language": "ger",
+    "percentage": 5.3
+  },
+  {
+    "genre": "Humor",
+    "language": "spa",
+    "percentage": 0.9
+  },
+  {
+    "genre": "Sciencefiction",
+    "language": "dut",
+    "percentage": 57.2
+  },
+  {
+    "genre": "Sciencefiction",
+    "language": "eng",
+    "percentage": 41.5
+  },
+  {
+    "genre": "Sciencefiction",
+    "language": "fre",
+    "percentage": 0.3
+  },
+  {
+    "genre": "Sciencefiction",
+    "language": "ger",
+    "percentage": 0.1
+  },
+  {
+    "genre": "Sciencefiction",
+    "language": "spa",
+    "percentage": 0.9
+  },
+  {
+    "genre": "Stripverhaal",
+    "language": "dut",
+    "percentage": 99.0
+  },
+  {
+    "genre": "Stripverhaal",
+    "language": "eng",
+    "percentage": 1.0
+  },
+  {
+    "genre": "Avontureroman",
+    "language": "dut",
+    "percentage": 69.8
+  },
+  {
+    "genre": "Avontureroman",
+    "language": "eng",
+    "percentage": 26.6
+  },
+  {
+    "genre": "Avontureroman",
+    "language": "fre",
+    "percentage": 1.1
+  },
+  {
+    "genre": "Avontureroman",
+    "language": "ger",
+    "percentage": 1.4
+  },
+  {
+    "genre": "Avontureroman",
+    "language": "spa",
+    "percentage": 1.1
+  },
+  {
+    "genre": "Romantisch Verhaal",
+    "language": "dut",
+    "percentage": 68.7
+  },
+  {
+    "genre": "Romantisch Verhaal",
+    "language": "eng",
+    "percentage": 26.1
+  },
+  {
+    "genre": "Romantisch Verhaal",
+    "language": "fre",
+    "percentage": 1.1
+  },
+  {
+    "genre": "Romantisch Verhaal",
+    "language": "ger",
+    "percentage": 2.4
+  },
+  {
+    "genre": "Romantisch Verhaal",
+    "language": "spa",
+    "percentage": 1.7
+  }
+]
 
-for(var i = 0; i < languageCount.length; i++){
-	if (languageCount[i] ["language"] === "dut"){
-		dutData.push(languageCount[i]);
-	}else if (languageCount[i] ["language"] === "eng"){
-		engData.push(languageCount[i]);
-	} else if (languageCount[i] ["language"] === "ger"){
-    gerData.push(languageCount[i]);
-  } else if (languageCount[i] ["language"] === "fre") {
-    freData.push(languageCount[i]);
+
+var dutData = []
+var engData = []
+var gerData = []
+var freData = []
+var spaData = []
+
+// Get data for the emtpy arrays above
+for (var i = 0; i < languageCount.length; i++) {
+  if (languageCount[i]["language"] === "dut") {
+    dutData.push(languageCount[i])
+  } else if (languageCount[i]["language"] === "eng") {
+    engData.push(languageCount[i])
+  } else if (languageCount[i]["language"] === "ger") {
+    gerData.push(languageCount[i])
+  } else if (languageCount[i]["language"] === "fre") {
+    freData.push(languageCount[i])
   } else {
-    spaData.push(languageCount[i]);
+    spaData.push(languageCount[i])
   }
 }
 
-//functions for toggling between data
-function change(value){
+// Functions for toggling between data
+// Value = value of radio buttons
+function change(value) {
 
-	if(value === 'dut'){
-		update(dutData);
-	}else if(value === 'eng'){
-		update(engData);
-	} else if(value === 'ger'){
-		update(gerData);
-	} else if(value === 'fre'){
-		update(freData);
-	} else if(value === 'spa'){
-		update(spaData);
-	} else if (value === 'all'){
-		update(languageCount);
-	}
+  if (value === "dut") {
+    update(dutData)
+  } else if (value === "eng") {
+    update(engData)
+  } else if (value === "ger") {
+    update(gerData)
+  } else if (value === "fre") {
+    update(freData)
+  } else if (value === "spa") {
+    update(spaData)
+  } else if (value === "all") {
+    update(languageCount)
+  }
 }
 
-function update(data){
-	//set domain for the x axis
-	xChart.domain(data.map(function(d){ return d.genre; }) );
-	//set domain for y axis
-	yChart.domain( [0, d3.max(data, function(d){ return +d.percentage; })] );
-  //yChart.domain([0, d3.max(languageCount, function(d){ return d.percent })])
-  //yChart.domain([0, 100])
+function update(data) {
+  // Set domain for the x axis
+  xChart.domain(data.map(function(d) {
+    return d.genre
+  }))
+  // Set domain for y axis
+  yChart.domain([0, d3.max(data, function(d) {
+    return +d.percentage
+  })])
 
-	//get the width of each bar
-	var barWidth = width / data.length;
-  console.log('chart: ', barWidth)
+  // Get the width of each bar
+  var barWidth = width / data.length
 
-	//select all bars on the graph, take them out, and exit the previous data set.
-	//then you can add/enter the new data set
+  // Select all bars on the graph, take them out, and exit dataset from before
+  // After that you can add the new data set with .enter()
   var bars = chart.selectAll(".bar")
-					.remove()
-					.exit()
-					.data(data)
-	//now actually give each rectangle the corresponding data
-	  .enter()
-		.append("rect")
-		.attr("class", "bar")
-		.attr("x", function(d, i){ return i * barWidth + 1 })
-		.attr("y", function(d){ return yChart(d.percentage) })
-		.attr("height", function(d){ return height - yChart(d.percentage) })
-		.attr("width", barWidth - 1)
-		.attr("fill", function(d){
-      if(d.language === "dut"){
-				return "rgba(122, 1, 119, 0.6)";
-			} else if (d.language === "eng"){
-				return "rgba(197, 27, 138, 0.6)";
-			} else if (d.language === "ger") {
-        return "rgba(247, 104, 161, 0.6)";
+    .remove()
+    .exit()
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d, i) {
+      return i * barWidth + 1
+    })
+    .attr("y", function(d) {
+      return yChart(d.percentage)
+    })
+    .attr("height", function(d) {
+      return height - yChart(d.percentage)
+    })
+    .attr("width", barWidth - 1)
+    .attr("fill", function(d) {
+      // Color for each language
+      if (d.language === "dut") {
+        return "rgba(122, 1, 119, 0.6)"
+      } else if (d.language === "eng") {
+        return "rgba(197, 27, 138, 0.6)"
+      } else if (d.language === "ger") {
+        return "rgba(247, 104, 161, 0.6)"
       } else if (d.language === "fre") {
-        return "rgba(251, 180, 185, 0.6)";
+        return "rgba(251, 180, 185, 0.6)"
       } else {
-        return "rgba(254, 235, 226, 0.6)";
+        return "rgba(254, 235, 226, 0.6)"
       }
-		})
-    // source tooltip: https://bl.ocks.org/alandunning/274bf248fd0f362d64674920e85c1eb7
-    .on("mousemove", function(d){
-    tooltip
-      .style("left", d3.event.pageX - 50 + "px")
-      .style("top", d3.event.pageY - 120 + "px")
-      .style("display", "inline-block")
-      .style("font-size", "20px")
-      .html((d.language) + "<br>" + (d.genre) + "<br>" + (d.percentage) + "%");
-})
-.on("mouseout", function(d){ tooltip.style("display", "none");});
+    })
+    // Source tooltip: https://bl.ocks.org/alandunning/274bf248fd0f362d64674920e85c1eb7
+    .on("mousemove", function(d) {
+      tooltip
+        .style("left", d3.event.pageX - 50 + "px")
+        .style("top", d3.event.pageY - 120 + "px")
+        .style("display", "inline-block")
+        .style("font-size", "20px")
+        .html((d.language) + "<br>" + (d.genre) + "<br>" + (d.percentage) + "%")
+    })
+    .on("mouseout", function(d) {
+      tooltip.style("display", "none")
+    })
 
-	//left axis
-	chart.select('.axis')
-		  .call(yAxis)
-	//bottom axis
-	chart.select('.xAxis')
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis)
-		.selectAll("text")
-			.style("text-anchor", "end")
-      .style("font-size", "20px")
-			.attr("dx", "-.8em")
-			.attr("dy", ".15em")
-			.attr("transform", function(d){
-				return "rotate(-65)";
-			})
+  // y axis
+  chart.select(".axis")
+    .call(yAxis)
+  // x axis
+  chart.select(".xAxis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .style("font-size", "20px")
+    .attr("dx", "-.8em")
+    .attr("dy", ".15em")
+    .attr("transform", function(d) {
+      return "rotate(-65)"
+    })
 
-      // Animation
-      chart.selectAll("rect")
-          .data(data)
-          .attr("y", 0)
-          .attr("height", 0)
-          .transition()
-          .ease(d3.easeBounce)
-          .delay(function (d) {return Math.random()*1000})
-          .duration(1000)
-          .attr("y", function(d) { return yChart(d.percentage) })
-          .attr("height", function(d) { return height - yChart(d.percentage) })
+  // Animation
+  // Credits: Guus told me that the height has to be 0 before you can do the transition
+  chart.selectAll("rect")
+    .data(data)
+    .attr("y", 0)
+    .attr("height", 0)
+    .transition()
+    .ease(d3.easeBounce)
+    .delay(function(d) {
+      return Math.random() * 1000
+    })
+    .duration(1000)
+    .attr("y", function(d) {
+      return yChart(d.percentage)
+    })
+    .attr("height", function(d) {
+      return height - yChart(d.percentage)
+    })
+
+    d3.select(".y")
+      .data(data)
+      .attr("y", 0)
+      .attr("height", 0)
+      .transition()
+      .ease(d3.easeLinear)
+      .delay(function(d) {
+        return Math.random() * 1000
+      })
+      .duration(1000)
+      .attr("y", function(d) {
+        return yChart(d.percentage)
+      })
+      .attr("height", function(d) {
+        return height - yChart(d.percentage)
+      })
+
 
 }
 
-//end update
+// End update
 
-//set up chart
-var margin = {top: 20, right: 20, bottom: 95, left: 50};
-var width = 1000;
-var height = 600;
+var margin = { top: 20, right: 20, bottom: 95, left: 100 }
+var width = 1000
+var height = 600
 
-var tooltip = d3.select("body").append("div").attr("class", "toolTip");
+var tooltip = d3.select("body").append("div").attr("class", "toolTip")
 
 var chart = d3.select(".chart")
-				.attr("width", width + margin.left + margin.right)
-				.attr("height", height + margin.top + margin.bottom)
-				.append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
 var xChart = d3.scaleBand()
-				.range([0, width])
+  .range([0, width])
 
 var yChart = d3.scaleLinear()
-				.range([height, 0]);
+  .range([height, 0])
 
 var xAxis = d3.axisBottom()
-            .scale(xChart)
-            .tickSize(10)
+  .scale(xChart)
+  .tickSize(10)
 
 var yAxis = d3.axisLeft()
-            .scale(yChart);
+  .scale(yChart)
 
-//set up axes
-//left axis
-	chart.append("g")
-		  .attr("class", "y axis")
-		  .call(yAxis)
-      .style("font-family", "Nexa Light, sans-serif")
-      .style("font-size", "12px")
+// Set up axes
+// y axis
+chart.append("g")
+  .attr("class", "y axis")
+  .call(yAxis)
+  .style("font-family", "Nexa Light, sans-serif")
+  .style("font-size", "12px")
 
-	//bottom axis
-	chart.append("g")
-		.attr("class", "xAxis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis)
-		.selectAll("text")
-      .style("font-size", "50px")
-			.style("text-anchor", "end")
-      .style("font-family", "Nexa Light, sans-serif")
-			.attr("dx", "-.8em")
-			.attr("dy", ".15em")
-			.attr("transform", function(d){
-				return "rotate(-65)";
-			})
+// x axis
+chart.append("g")
+  .attr("class", "xAxis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis)
+  .selectAll("text")
+  .style("font-size", "50px")
+  .style("text-anchor", "end")
+  .style("font-family", "Nexa Light, sans-serif")
+  .attr("dx", "-.8em")
+  .attr("dy", ".15em")
+  .attr("transform", function(d) {
+    return "rotate(-65)"
+  })
 
-//add labels
+// Labels y and x axis
 chart
-	.append("text")
-	.attr("transform", "translate(-35," +  (height+margin.bottom)/2 + ") rotate(-90)")
+  .append("text")
+  .attr("transform", "translate(-35," + (height + margin.bottom) / 2 + ") rotate(-90)")
   .style("fill", "#ffffff")
   .style("font-family", "Nexa Bold, sans-serif")
   .style("font-size", "18px")
-	.text("% boeken")
+  .text("% boeken")
 
 chart
-	.append("text")
-	.attr("transform", "translate(" + (width/2) + "," + (height + margin.bottom + 100) + ")")
+  .append("text")
+  .attr("transform", "translate(" + (width / 2) + "," + (height + margin.bottom + 100) + ")")
   .style("fill", "#ffffff")
   .style("font-family", "Nexa Bold, sans-serif")
   .style("font-size", "18px")
-	.text("Genres")
+  .text("Genres")
 
 d3.select("svg")
-    // .style("width", 1000)
-    .style("margin-left", "30px")
-    .style("padding-left", "0")
-    .style("height", "1000px")
+  .style("margin-left", "30px")
+  .style("padding-left", "0")
+  .style("height", "1000px")
 
-//use languageCount to begin with
-update(languageCount);
-})
-//})
+// Use languageCount to begin with
+update(languageCount)
--- stackedBarChart2.js
+++ stackedBarChart2.js
@@ -7,93 +7,111 @@ function process(d) {
 	return processed
 }
 
-const svg = d3.select("svg")
-const margin = {top: 20, right: 60, bottom: 30, left: 40}
-const width = +svg.attr("width") - margin.left - margin.right
-const height = +svg.attr("height") - margin.top - margin.bottom
-const g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-const x = d3.scaleBand()
+var x = d3.scaleBand()
     .rangeRound([0, width])
-    .paddingInner(0.1)
-    .align(0.2)
+    .paddingInner(0.05)
+    .align(0.1);
 
 var y = d3.scaleLinear()
-    .range([height, 0])
+    .rangeRound([height, 0]);
 
 var z = d3.scaleOrdinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-var stack = d3.stack()
-    .order(d3.stackOrderNone)
-    .offset(d3.stackOffsetExpand)
+    d3.json("data.json").then(function(data) {
 
-d3.json("data.json").then(function(data) {
-  const dataMap = data.map(process)
-  console.log("DataMap:", dataMap)
-  // console.log("data", data)
-  const languageCount  = d3.nest()
-    // .key(function(d) { return d.publicationYear})
-    .key(function(d) { return d.genre })
-    .key(function(d) { return d.language })
-    .rollup(function(v) { return v.length })
-    .entries(data)
-    .filter(genre => genre.key !== "Stripverhaal")
-    console.log(languageCount)
+      const dataMap = data.map(process)
+      console.log('DataMap:', dataMap)
+      // console.log("data", data)
+      const languageCount = d3.nest()
+        // .key(function(d) { return d.publicationYear})
+        .key(function(d) { return d.genre })
+        .key(function(d) { return d.language })
+        .rollup(function(v) { return Number(v.length) })
+        .entries(data)
+        // .filter(genre => genre.key !== "Stripverhaal")
 
-    data.forEach(function(d){d.total=d.dut+d.eng+d.fre+d.ger+d.spa})
-    data.sort(function(d) { return d.value })
+        const objWithPercentage = languageCount.map(obj => {
+          let total = 0
+          obj.values.map(v => total += v.value)
 
-     x.domain(languageCount.map(function(d) { return d.key; }))
-     z.domain(["dut", "eng", "fre", "ger", "spa"]);
+          return {
+              ...obj,
+              values: obj.values.map(ov => ({
+                ...ov,
+                percent: ov.value / total * 100
+              })),
+              total,
+          }
+        })
+        console.log("languageCount: ", objWithPercentage)
 
-  const serie = g.selectAll(".serie")
-    .data(stack.keys(["dut", "eng", "fre", "ger", "spa"])(languageCount))
+  // var keys = languageCount.slice();
+  // console.log(keys)
+
+  data = languageCount.filter(s => ['Detective', 'Humor', 'Sciencefiction', "Stripverhaal", "Avontureroman", "Romantisch verhaal"].includes(s.key))
+  console.log(data)
+
+  data.sort(function(a, b) { return b.value - a.value; });
+  x.domain(languageCount.map(function(d) { return d.key; }));
+  // y.domain([0, d3.max(languageCount, function(d) { return d.values; })]).nice();
+  y.domain([0, 100])
+  // z.domain(keys);
+
+  g.append("g")
+    .selectAll("g")
+    .data(d3.stack()(languageCount))
     .enter().append("g")
-     .attr("class", "serie")
-     // .attr("fill", "lightgreen")
-     .attr("fill", function(d) { return z(d.language) })
-
-     serie.selectAll("rect")
+      .attr("fill", function(d) { return z(d.key); })
+    .selectAll("rect")
     .data(languageCount)
     .enter().append("rect")
-      .attr("x", x.bandwidth())
-      //function(d) { return d.key })
-      // function(d) { console.log("keys: ", d.key)
-      //   return x(d.key); })
-      .attr("y", function(d) {
-        return d.values[0].value &&
-              d.values[1].value &&
-              d.values[2].value &&
-              d.values[3].value &&
-              d.values[4].value
-      })
-      // function(d) {
-      //   // console.log(d.values[0].value, d.values[1].value)
-      //   return d.values[0].value,
-      //         d.values[1].value
-      //   })
-      .attr("height", function(d) {
-        return d.values[0].value &&
-              d.values[1].value &&
-              d.values[2].value &&
-              d.values[3].value &&
-              d.values[4].value
-       })
+      .attr("x", function(d) { return x(d.key); })
+      .attr("y", function(d) { return y(d[1]); })
+      .attr("height", function(d) { return y(d[0]) - y(d[1]); })
       .attr("width", x.bandwidth());
 
   g.append("g")
-      .attr("class", "axis axis--x")
+      .attr("class", "axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
+      .call(d3.axisBottom(x));
 
   g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10, "%"))
+      .attr("class", "axis")
+      .call(d3.axisLeft(y).ticks(null, "s"))
+    .append("text")
+      .attr("x", 2)
+      .attr("y", y(y.ticks().pop()) + 0.5)
+      .attr("dy", "0.32em")
+      .attr("fill", "#000")
+      .attr("font-weight", "bold")
+      .attr("text-anchor", "start")
+      .text("Percentage");
 
+  var legend = g.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+      .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(languageCount.slice().reverse())
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-  // const legend = serie.append("g")
-  //     .attr("class", "legend")
-  //     .attr("transform", function(d) {
-  //      var d = d[d.length - 1]; return "translate(" + (x(d.key) + x.bandwidth()) + "," + (d.value) + ")"; });
-  })
+  legend.append("rect")
+      .attr("x", width - 19)
+      .attr("width", 19)
+      .attr("height", 19)
+      .attr("fill", z);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9.5)
+      .attr("dy", "0.32em")
+      .text(function(d) { return d; });
+});
